799e457e1d820e8ca616fb16d67f0bcfcc877ad5
Coodie <65614373+CodyRobbins99@users.noreply.github.com>
2020-10-10 09:22:27 -0400

    service worker and manifest added

---
+++ public/service-worker.js
@@ -0,0 +1,71 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/css/style.css',
+  '/js/index.js',
+  '/js/idb.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png',
+];
+
+self.addEventListener('install', function(evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Your files were pre-cached successfully!');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function (evt) {
+    console.log('fetch request : ' + evt.request.url)
+    evt.respondWith(
+        caches.match(evt.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                fetch(e.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        caches.open(CACHE_NAME).then(cache => {
+                            cache.put(e.request.url, response.clone());
+                        });
+                    }
+                    return response;
+                });
+            }
+        })
+    )
+});
